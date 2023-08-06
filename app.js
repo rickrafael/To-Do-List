109d3fa2a37b13ccb440e9fcca10dd32375e0cb4
Ricardo Rafael <130177490+rickrafael@users.noreply.github.com>
2023-08-06 17:01:51 -0300

    readme
    readme

---
+++ app.js
@@ -0,0 +1,24 @@
+//obtendo referências
+const form = document.querySelector('.form-add-todo')
+const ul = document.querySelector('ul')
+
+//adicionando listener de evento
+form.addEventListener('submit', event=> {
+  event.preventDefault()
+
+  const inputValue = event.target.add.value.trim()
+
+  if(inputValue.length){
+  ul.innerHTML += `
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+  `
+  }
+  
+  event.target.reset()
+
+ 
+})
+
